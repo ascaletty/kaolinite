efe8cf53a703c933fa6c0801c6a193ed991e2368
Aaron Scaletty <ascaletty23@gmail.com>
2025-06-04 10:15:16 -0500

    still working on text edit added some execution changes
    a

--- main.c
+++ main.c
@@ -1,23 +1,65 @@
 #define CLAY_IMPLEMENTATION
-#include "clay.h"
+#include "clay/clay.h"
 #include "raylib/clay_renderer_raylib.c"
 #include "stdio.h"
 #include "unistd.h"
 #define MAX_CHARS 100
 char input[MAX_CHARS+1]= "\0";
-int letterCount= 1;
-
-
+int CursorPos= 0;
+int size=1;
+struct greaterSmaller {
+    int pos, size;
+};
+typedef struct greaterSmaller Struct;
 void HandleClayErrors(Clay_ErrorData errorData) {
     printf("%s", errorData.errorText.chars);
 }
-Clay_Color COLOR_BACKGROUND= {25, 116, 242, 255};
-Clay_Color COLOR_INPUT= {221,110,15,255};
+
+
+Clay_Color COLOR_BACKGROUND= {38, 152, 152, 255};
+Clay_Color COLOR_INPUT= {203,4,4,230};
+Clay_Color COLOR_HOVERED= {203,4,4,255};
 Clay_Color COLOR_EVEN= {194,207,178,250};
 Clay_Color COLOR_ODD= {126,137,135,250};
+
+int insertText(char input[], int size, int pos, int val, bool left, bool right){
+ 
+  if(left == true){
+  for(int i= size; i>pos; i--){
+    //printf("in inserText");
+    input[i]= input[i-1];
+  } 
+  
+  }
+  else if (right= true){
+
+  }
+  else{
+   for(int i= size; i>pos; i--){
+    //printf("in inserText");
+    input[i]= input[i-1];
+  }
+  input[pos]= val;
+
+  size++;
+
+  }
+  return size;
+}
+Struct backspace(char input[], int size, int pos){
+  input[pos-1]= '|';
+  input[pos]= '\0';
+  pos--;
+  size--;
+Struct s;
+  s.size= size;
+  s.pos= pos;
+  return s;
+}
 void showProgram(Clay_String programName){
 CLAY({
-    .backgroundColor= COLOR_INPUT,
+    .id= CLAY_ID("INPUT"),
+    .backgroundColor= Clay_Hovered() ? COLOR_HOVERED: COLOR_INPUT,
     .layout= {.sizing={.width=CLAY_SIZING_GROW()}}
   }) {
 CLAY_TEXT(programName, CLAY_TEXT_CONFIG({ 
@@ -26,47 +68,64 @@ CLAY_TEXT(programName, CLAY_TEXT_CONFIG({
           .textColor= {255,255,255,255}}));
   }
 }
-Clay_String HandleTypinginput(int key, bool keyDown){
-  if(letterCount<=0){
-    letterCount= 1;
+Clay_String HandleTypinginput(int key,int keyDown){
+  if(CursorPos<=0){
+    CursorPos= 1;
   }
-  if(letterCount>=100){
-    input[0]= '\0';
-    letterCount=0;
+  if(CursorPos>=100){
+    CursorPos=0;
   }
-  while((letterCount>=0) && (key>0)){
-    input[letterCount]= (char) key;
-    input[letterCount+1]= '\0';
-  
-    printf("Value of input = %c\n", input[letterCount]);
-    printf("letterCount: %d\n", letterCount);
-    Clay_String name;
-    name.length= letterCount+1;
-    //for(int i=0; i<letterCount+1;i++){
-    name.chars= &input[letterCount];
-    //printf("i value:%d\n",i);
-    //}
+  if(size<=0){
+    size=1;
+  }
+  input[CursorPos]= '|';
+  while((CursorPos>=0) && (key>0)){
+     Clay_String name;
+    input[CursorPos]= (char) key;
+    size=insertText(input, CursorPos, CursorPos, input[CursorPos],false, false);
+    printf("size %d\n", size);
+    name.length= size;
+    name.chars= &input[CursorPos];
     name.isStaticallyAllocated= true;
-    letterCount++;
+    CursorPos++;
     return name;
   }
   Clay_String keepname;
-  if(keyDown== true){
-  letterCount--;
-  input[letterCount]= '\0';  }
-  keepname.length= letterCount+1;
+
+  //when backspace is pressed 
+  if(keyDown==0){
+  Struct context;
+  context = backspace(input,size,CursorPos);
+  CursorPos= context.pos;
+  size= context.size;
+  printf("size %d\n",size);
+  printf("CursorPos %d\n",CursorPos);
+
+  }
+  else if(keyDown==1){
+  CursorPos--;
+  size=insertText(input, size, CursorPos, '|', true, false);
+  }
+  else if(keyDown==2){
+  input[size+1]= '\0';
+  printf("%c\n",input); 
+  const char* pinput= &input;
+  system(input); 
+  }
+  keepname.length= size;
   keepname.chars= &input[1];
   keepname.isStaticallyAllocated= true;
-  return keepname; 
+  return keepname;
 }
 
 
 int main(void){
 	Clay_Raylib_Initialize(500, 400, "test", FLAG_WINDOW_RESIZABLE);
+  //SetTargetFPS(2);
 	uint64_t clayRequiredMemory= Clay_MinMemorySize();
 	Clay_Arena clayMemory = (Clay_Arena) {
 		.memory = malloc(clayRequiredMemory),
-                .capacity = clayRequiredMemory
+    .capacity = clayRequiredMemory
 	};
 	Clay_Initialize(clayMemory, (Clay_Dimensions){
 	.width = GetScreenWidth(),
@@ -76,16 +135,33 @@ int main(void){
 	fonts[0] = LoadFontEx("resources/Roboto-Regular.ttf", 48, 0, 400);
 	Clay_SetMeasureTextFunction(Raylib_MeasureText, fonts);
 	while(!WindowShouldClose()){ 
+      Vector2 mousePosition = GetMousePosition();
+      Clay_SetPointerState(
+      (Clay_Vector2) {mousePosition.x,mousePosition.y},
+      IsMouseButtonDown(0)
+      );
+
     //run every frame
     int key=GetCharPressed(); 
-    bool keyDown= false;
+    int keyDown=3;
+
     if(IsKeyPressed(KEY_BACKSPACE)){
-      keyDown= true;
+      keyDown= 0;
+    }
+    else if(IsKeyPressed(KEY_LEFT)){
+      keyDown=1;
+    }
+    else if(IsKeyPressed(KEY_ENTER)){
+      keyDown=2;
     }
     Clay_String p1 =HandleTypinginput(key, keyDown);
 
-	Clay_BeginLayout();
-	CLAY({ .id = CLAY_ID("OuterContainer"), .layout = {.layoutDirection= CLAY_TOP_TO_BOTTOM, .sizing = {CLAY_SIZING_GROW(0), CLAY_SIZING_GROW(0)}, .padding = CLAY_PADDING_ALL(16), .childGap = 16 }, .backgroundColor = COLOR_BACKGROUND }){
+  Clay_BeginLayout();
+	CLAY({ .id = CLAY_ID("OuterContainer"), 
+      .layout = {.layoutDirection= CLAY_TOP_TO_BOTTOM, 
+      .sizing = {CLAY_SIZING_GROW(0), CLAY_SIZING_GROW(0)}, 
+        .padding = CLAY_PADDING_ALL(16), .childGap = 16 }, 
+        .backgroundColor = COLOR_BACKGROUND }){
       {
         showProgram(p1);
       }
