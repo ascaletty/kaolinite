40d98bb082e6b8ec7121f5a58643f4b4e091c398
Aaron Scaletty <ascaletty23@gmail.com>
2025-06-15 01:20:40 -0500

    path bash script complete pretty much just need to implement in clay

--- kaolinite_path.sh
+++ kaolinite_path.sh
@@ -7,9 +7,11 @@ echo $size
 declare -A applications
 i=0
 until [ $i -ge $size ]; do
-  awk -F'=' '/^Name=/ { print $2}' "${dmenucacheraw[$i]}"
-  awk -F'=' '/^Exec=/ { print $2 }' "${dmenucacheraw[$i]}"
-  echo $value
-  applications[$key]=$value
+  # awk -F'=' '/^Name=/ { print $2; exit}' "${dmenucacheraw[$i]}"
+  # awk -F'=' '/^Exec=/ { print $2; exit }' "${dmenucacheraw[$i]}"
+  applications[$(awk -F'=' '/^Name=/ { print $2; exit}' "${dmenucacheraw[$i]}")]=$(awk -F'=' '/^Exec=/ { print $2; exit }' "${dmenucacheraw[$i]}" | cut -d " " -f1)
   ((i++))
+done
+for key in "${!applications[@]}"; do
+  printf "$key,${applications[$key]}," >>$HOME/.cache/kaolinitedrun.cache
 done
